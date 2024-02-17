bd1160cd82d0da9ec434ccc2e88bd0f3f90e91ea
Bùi Trung Kiên <73433399+Kien4711@users.noreply.github.com>
2024-02-17 20:41:42 +0700

    Add files via upload
    answer for the housenow test

---
+++ backend-take-home-assignment/src/server/api/routers/my-friend-router.ts
@@ -0,0 +1,86 @@
+import type { Database } from '@/server/db'
+
+import { TRPCError } from '@trpc/server'
+import { z } from 'zod'
+
+import { FriendshipStatusSchema } from '@/utils/server/friendship-schemas'
+import { protectedProcedure } from '@/server/trpc/procedures'
+import { router } from '@/server/trpc/router'
+import {
+  NonEmptyStringSchema,
+  CountSchema,
+  IdSchema,
+} from '@/utils/server/base-schemas'
+
+export const myFriendRouter = router({
+  getById: protectedProcedure
+    .input(
+      z.object({
+        friendUserId: IdSchema,
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.connection().execute(async (conn) =>
+        conn
+          .selectFrom('users as friends')
+          .innerJoin('friendships', 'friendships.friendUserId', 'friends.id')
+          .innerJoin(
+            userTotalFriendCount(conn).as('userTotalFriendCount'),
+            'userTotalFriendCount.userId',
+            'friends.id'
+          )
+          .where('friendships.userId', '=', ctx.session.userId)
+          .where('friendships.friendUserId', '=', input.friendUserId)
+          .where(
+            'friendships.status',
+            '=',
+            FriendshipStatusSchema.Values['accepted']
+          )
+          // Calculate mutual friend count using a subquery
+          .select([
+            'friends.id',
+            'friends.fullName',
+            'friends.phoneNumber',
+            'totalFriendCount',
+            // Subquery to calculate mutual friend count
+            conn.subQuery()
+              .select(eb.fn.countDistinct('friendships.userId'))
+              .from('friendships')
+              .where('friendships.friendUserId', '=', ctx.session.userId)
+              .andWhere(
+                'friendships.userId',
+                '=',
+                input.friendUserId
+              )
+              .andWhere(
+                'friendships.status',
+                '=',
+                FriendshipStatusSchema.Values['accepted']
+              )
+              .as('mutualFriendCount')
+          ])
+          .executeTakeFirstOrThrow(() => new TRPCError({ code: 'NOT_FOUND' }))
+          .then(
+            z.object({
+              id: IdSchema,
+              fullName: NonEmptyStringSchema,
+              phoneNumber: NonEmptyStringSchema,
+              totalFriendCount: CountSchema,
+              mutualFriendCount: CountSchema,
+            }).parse
+          )
+      )
+    }),
+})
+
+
+const userTotalFriendCount = (db: Database) => {
+  return db
+    .selectFrom('friendships')
+    .where('friendships.status', '=', FriendshipStatusSchema.Values['accepted'])
+    .select((eb) => [
+      'friendships.userId',
+      eb.fn.count('friendships.friendUserId').as('totalFriendCount'),
+    ])
+    .groupBy('friendships.userId')
+}
